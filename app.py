f0c9491e09862a8c1a99d315bbb0a787af23beb2
iruy-fr <iruy.fr@gmail.com>
2024-09-23 08:12:49 -0300

    Atualização de programa

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import tkinter
-import customtkinter
 import subprocess
-import os
 
-
-def startDisconnect():
-    target_user = user.get()
+def startDisconnect(target_user, progress, success_label):
     host_list = [
         '172.16.0.201',
         '172.16.0.202',
@@ -15,69 +10,47 @@ def startDisconnect():
         '172.16.0.206'
     ]
 
+    user_found = False  # Variável para rastrear se o usuário foi encontrado
+
     for host in host_list:
-        progress.start()
+        progress.set(value=0)
         progress.set(value=25)
         try:
-            #Caminho para o script para encontrar/resetar usuário
+            # Caminho para o script para encontrar/resetar usuário
             script_path = "find_sessionid.ps1"
             reset_path = "reset_session.ps1"
             command = [
                 "powershell.exe",
-                 "-WindowStyle",
-                 "Hidden",
-                 "-NoProfile",
-                 "-ExecutionPolicy",
-                 "Bypass",
-                 "-File",  script_path,
-                 '-arg_user', target_user.lower(),
-                 '-arg_host',host
+                "-File", script_path,
+                '-arg_user', target_user.lower(),
+                '-arg_host', host
             ]
+            progress.set(value=50)
             result = subprocess.run(command,
                                     capture_output=True,
                                     creationflags=subprocess.CREATE_NO_WINDOW)
-            data = int(result.stdout.decode('utf-8'))
-            command_kill = [
-                "powershell.exe",
-                "-WindowStyle",
-                "Hidden",
-                "-NoProfile",
-                "-ExecutionPolicy",
-                "Bypass",
-                "-File", reset_path,
-                '-sessionId', str(data),
-                '-arg_host', host
-            ]
-            subprocess.run(command_kill,
-                           capture_output=True,
-                           creationflags=subprocess.CREATE_NO_WINDOW)
+            data = result.stdout.decode('utf-8').strip()
+
+            # Verifica se o usuário foi encontrado (se a saída contiver um sessionId válido)
+            if data.isdigit():
+                command_kill = [
+                    "powershell.exe",
+                    "-File", reset_path,
+                    '-sessionId', str(data),
+                    '-arg_host', host
+                ]
+                progress.set(value=75)
+                subprocess.run(command_kill,
+                               capture_output=True,
+                               creationflags=subprocess.CREATE_NO_WINDOW)
+                progress.set(value=100)
+                success_label.configure(text=f"Usuário {target_user} \n desconectado do servidor {host}")
+                user_found = True  # Usuário foi encontrado e desconectado
+                break  # Para de procurar em outros hosts após desconectar o usuário
+
         except Exception as e:
-            print(f"Usuário não encontrado no servidor {host}")
-            progress.set(value=100)
-        progress.stop()
+            print(f"Erro ao desconectar usuário no servidor {host}: {e}")
 
-
-#System settings
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
-#App Frame
-app = customtkinter.CTk()
-app.geometry("300x250")
-app.title("Desconectar Usuário")
-font = customtkinter.CTkFont(family="Cascadia", size=16)
-#UI Elements
-#Texto Inserir usuário
-insiratxt = customtkinter.CTkLabel(app, text="Insira seu usuário:")
-insiratxt.pack(padx=10, pady=20, anchor=tkinter.CENTER)
-#Input usuário
-user = customtkinter.CTkEntry(app, width=150, height=10)
-user.pack(padx=5, pady=5, anchor=tkinter.CENTER)
-#Texto Desconectar
-disconnect = customtkinter.CTkButton(app, text="Desconectar", command=startDisconnect)
-disconnect.pack(padx=5, pady=5, anchor=tkinter.CENTER)
-#Barra de Progresso
-progress = customtkinter.CTkProgressBar(app)
-progress.pack(padx=5, pady=10, anchor=tkinter.CENTER)
-progress.set(0)
-#Run App
-app.mainloop()
+    # Se o usuário não foi encontrado em nenhum servidor
+    if not user_found:
+        success_label.configure(text=f"Usuário {target_user} \n não encontrado em nenhum servidor")
